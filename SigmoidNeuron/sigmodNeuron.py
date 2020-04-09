bb444c8e0c966bdd6ca0217f2e5c5e11bbc1d901
Sudhakar <sramakrishnan@admins-MacBook-Pro.local>
2020-04-09 17:48:26 +0530

    Updates for SimoidNeuron

--- SigmoidNeuron/sigmodNeuron.py
+++ SigmoidNeuron/sigmodNeuron.py
@@ -6,56 +6,68 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 
 
-X = [0.5, 2.5]
-Y = [0.2, 0.9]
+class SigmoidNeuron:
+    def __init__(self):
+        self.w = None
+        self.b = None
+
+    def perceptron(self, x):
+        return np.dot(x, self.w.T) + self.b
+
+    # gives graded output
+
+    def sigmoid(self, x):
+        return 1.0 / (1.0 + np.exp(-x))
+
+    # function that computes derivative of loss fn w.r.t b
+
+    def grad_b(self, x, y):
+        y_pred = self.sigmoid(self.perceptron(x))
+        return (y_pred - y) * y_pred * (1 - y_pred) * x
+
+    # function that computes derivative of loss fn w.r.t w
+    # gradient descent learning algorithm
+    def grad_w(self, x, y):
+        y_pred = self.sigmoid(self.perceptron(x))
+        return (y_pred - y) * y_pred * (1 - y_pred)
+
+    def fit(self, X, Y, epochs=1, learning_rate=1, initialize=True):
+
+        # initialize w, b
+        if initialize:
+            self.w = np.random.randn(1, X.shape[1])
+            self.b = 0
+
+        # iterate over the data
+        # compute yhat
+        # compute loss with w, b
+        # if not happy with loss, adjust w, b
+        # till satisfied
+
+        for i in range(epochs):
+            # compute gradient
+            dw = 0
+            db = 0
+            # go thru inputs
+            for x, y in zip(X, Y):
+                # derivate of loss function w.r.t w
+                dw += self.grad_w(x, y)
+                # derivate of loss function w.r.t b
+                db += self.grad_b(x, y)
+            self.w -= learning_rate * dw
+            self.b = learning_rate * db
+        return self
 
 
-def sigmoid_f(w, b, x):
-    # sigmoid with parameters w,b
-    return 1.0 / (1.0 + np.exp(-(w*x + b)))
+# linearly seperable data
+X = np.asarray([[2.5, 2.5], [4, -1],  [1, -4], [3, 1.25], [2, 4], [1, 5]])
+# Ground truth data
+Y = [1, 1, 1, 0, 0, 0]
 
 
-# goes over the data points in all of X, Y
-# compute the true value of fn and calculate error loss
-
-def error(w, b):
-    error = 0.0
-    for x, y in zip(X, Y):
-        fx = sigmoid_f(w, b, x)
-        err += 0.5 * (fx - y) ** 2
-    return err
-
-
-# function that computes derivative of loss fn w.r.t b
-def grad_b(w, b, x, y):
-    fx = sigmoid_f(w, b, x)
-    return (fx - y) * fx * (1 - fx)
-
-# function that computes derivative of loss fn w.r.t w
-
-
-def grad_w(w, b, x, y):
-    fx = sigmoid_f(w, b, x)
-    return (fx - y) * fx * (1 - fx) * x
-
-
-# main function
-def do_gradient_descent():
-    # initialize w,b , learning rate as 1.0
-    w, b, eta = -2, -2, 1.0
-    max_epochs = 1000  # 1000 passes over my data
-
-    # compute derivatives and update weights in each pass
-    for i in range(max_epochs):
-        dw, db = 0, 0
-        for x, y in zip(X, Y):
-            dw += grad_w(w, b, x, y)
-            db += grad_b(w, b, x, y)
-        plt.plot(dw, db)
-    w = w - eta * dw
-    b = b - eta * db
-
-
-plt.plot(X, Y)
-plt.show()
-do_gradient_descent()
+# instantiate a sigmoid neuron
+sn = SigmoidNeuron()
+sn.fit(X, Y, 1, 0.25, True)
+for i in range(10):
+    print(sn.w, sn.b)  # weight and bias
+    sn.fit(X, Y, 1, 0.25, False)
--- SigmoidNeuron/supportFunctions.py
+++ SigmoidNeuron/supportFunctions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors
 from mpl_toolkits import mplot3d
 
 
@@ -43,10 +44,19 @@ X2 = np.linspace(-10, 10, 100)
 XX1, XX2 = np.meshgrid(X1, X2)
 print(X1.shape, X2.shape, XX1.shape, XX2.shape)
 
-w1 = 0.5
+w1 = 2
 w2 = 0.5
 b = 0
 Y = sigmoid_2d(XX1, XX2, w1, w2, b)
+
+
+my_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+    "", ["red", "yellow", "green"])
+# Contour plot example
+# alpha introduces transparency and grid lines
+# faster transition is shown by a thinner transition
+plt.contourf(XX1, XX2, Y, cmap=my_cmap, alpha=0.6)
+plt.show()
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
@@ -54,9 +64,60 @@ ax.plot_surface(XX1, XX2, Y, cmap='viridis')
 ax.set_xlabel('X1')
 ax.set_ylabel('X2')
 ax.set_zlabel('Y')
-# plt.show()
+
 
 # create a slice of the graph and watch the curve surface as a sigmoid
-ax.view_init(30, 270)
-fig
+# ax.view_init(30, 270)
+
+# see dominating aspect of X2
+ax.view_init(30, 180)
+# plt.show()
+
+
+# how to computer loss for a given dataset
+w_unknown = 0.5
+b_unknown = 0.25
+X = np.random.random(25) * 20 - 10
+Y = sigmoid(X, w_unknown, b_unknown)
+
+
+def calculate_loss(X, Y, w_est, b_est):
+    loss = 0
+    for x, y in zip(X, Y):
+        loss += (y - sigmoid(x, w_est, b_est)) ** 2
+    return loss
+
+
+plt.plot(X, Y, '*')
+# plt.show()
+
+W = np.linspace(0, 2, 100)
+B = np.linspace(-1, 1, 100)
+WW, BB = np.meshgrid(W, B)
+Loss = np.zeros(WW.shape)
+print(WW.shape)
+for i in range(WW.shape[0]):
+    for j in range(WW.shape[1]):
+        Loss[i, j] = calculate_loss(X, Y, WW[i, j], BB[i, j])
+
+
+# z axis is loss
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_surface(WW, BB, Loss, cmap='viridis')
+ax.set_xlabel('w')
+ax.set_ylabel('b')
+ax.set_zlabel('Loss')
+ax.view_init(30, 180)  # see from bias perspective
 plt.show()
+
+# we want to find min w, b
+# seeing the graph, we observe -w is bad, so refine the space of W
+# change W = np.linspace(-1, 1, 100) to
+# W = np.linspace(0, 2, 100)
+ij = np.argmin(Loss)
+i = int(np.floor(ij/Loss.shape[1]))
+j = int(ij-i * Loss.shape[1])
+
+print(i, j)
+print(WW[i, j], BB[i, j])
